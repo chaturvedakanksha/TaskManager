47b845ce40795fa4b3c8d49c4418e6fd62ebb5ff
chaturvedakanksha <chturvedakanksha@gmail.com>
2024-07-15 13:25:45 +0530

    Adding console application for migration

---
+++ TaskManageConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TaskManager.DbContext;
+using TaskManager.Models;
+
+DbContextOptions<TaskManagementDbContext> options = new DbContextOptionsBuilder<TaskManagementDbContext>()
+  .UseSqlServer("Server=DESKTOP-LA9385J;Database=TaskManager;Integrated Security=True;")
+  .Options;
+
+
+using (var context = new TaskManagementDbContext(options))
+{
+    //creates db if not exists
+    context.Database.EnsureCreated();
+    var task = new Ticket
+    {
+        Title = "Task 1",
+        Description = "Task 1 Description",
+        EmployeeId = 1,
+        Status = TicketStatus.Pending,
+        StartDate = DateTime.Now
+    };
+    context.Tickets.Add(task);
+    context.SaveChanges();
+    Console.WriteLine("Task added successfully");
+    Console.ReadLine();
+}
--- TaskManager/Models/Task.cs
+++ TaskManager/Models/Task.cs
@@ -15,10 +15,14 @@ namespace TaskManager.Models
         public string Title { get; set; }
         public string? Description { get; set; }
         public int? EmployeeId { get; set; }
+        //reference navigation property : will create a foreign key of employee id in ticket table
         public Employee? Employee { get; set; }
         public TicketStatus? Status { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? CompletionDate { get; set; }
+        
+        //collection navigation property : will create a many relationship between ticket and notes
+        //It will create a TicketId in Note table
         public ICollection<Note>? Notes { get; set; }
         public ICollection<Document>? Documents { get; set; }
     }
